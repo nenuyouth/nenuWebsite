8b18442e5e4ea80b2f85172c893f988ea333eda7
Mr.Hope <zhangbowang1998@gmail.com>
2019-05-07 19:41:32 +0800

    更新单元测试工具

--- jest.config.js
+++ jest.config.js
@@ -12,26 +12,14 @@ module.exports = {
     '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
     '^.+\\.tsx?$': 'ts-jest'
   },
-  transformIgnorePatterns: [
-    '/node_modules/'
-  ],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
-  },
-  snapshotSerializers: [
-    'jest-serializer-vue'
-  ],
-  testMatch: [
-    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
-  ],
+  transformIgnorePatterns: ['/node_modules/'],
+  moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
+  snapshotSerializers: ['jest-serializer-vue'],
+  testMatch: ['**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'],
   testURL: 'http://localhost/',
   watchPlugins: [
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname'
   ],
-  globals: {
-    'ts-jest': {
-      babelConfig: true
-    }
-  }
-}
+  globals: { 'ts-jest': { babelConfig: true } }
+};
