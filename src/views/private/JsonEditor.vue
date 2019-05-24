36ae5c30287753fa1fbfd681ceaf998a07249a3f
Mr.Hope <zhangbowang1998@gmail.com>
2019-05-24 23:04:07 +0800

    升级依赖

--- src/views/private/JsonEditor.vue
+++ src/views/private/JsonEditor.vue
@@ -2,7 +2,7 @@
  * @Author: Mr.Hope
  * @Date: 2019-05-19 17:25:48
  * @LastEditors: Mr.Hope
- * @LastEditTime: 2019-05-24 15:01:37
+ * @LastEditTime: 2019-05-24 22:22:21
  * @Description: Json Editor
 -->
 <template>
@@ -12,237 +12,76 @@
         <!-- 循环pageJson -->
 
         <!-- 带有分割线、下拉列表的标题 -->
-        <DropdownTitle
-          :key="partIndex"
-          :menu="tagList"
-          :onChange="tagChange(partIndex)"
-          v-model="tags[partIndex]"
-        />
+        <DropdownTitle :key="partIndex" :menu="tagList" v-model="tags[partIndex]"/>
 
         <!-- 选项列表 -->
         <template v-if="part!=='请选择'">
           <template v-for="config in Object.keys(configuration[part])">
             <!-- 每个选项设置 -->
 
+            <!-- 联合类型输入 -->
+            <form-union-input
+              :configuration="configuration[part][config]"
+              :identifier="`${partIndex}-${config}`"
+              :key="`${partIndex}-${config}`"
+              v-if="typeof configuration[part][config].type==='object'"
+            />
+
+            <!-- 可遍历值输入 -->
+            <form-enum-input
+              :configuration="configuration[part][config]"
+              :identifier="`${partIndex}-${config}`"
+              :key="`${partIndex}-${config}`"
+              v-else-if="configuration[part][config].enum"
+            />
+
             <!-- 布尔值输入 -->
             <form-boolean-input
-              :config="config"
               :configuration="configuration[part][config]"
-              :index="partIndex"
+              :identifier="`${partIndex}-${config}`"
               :key="`${partIndex}-${config}`"
-              v-if="configuration[part][config].type==='boolean'"
+              v-else-if="configuration[part][config].type==='boolean'"
             />
 
             <!-- 数字输入 -->
             <form-number-input
-              :config="config"
               :configuration="configuration[part][config]"
-              :index="partIndex"
+              :identifier="`${partIndex}-${config}`"
               :key="`${partIndex}-${config}`"
               v-else-if="configuration[part][config].type==='number'"
             />
 
             <!-- 单行输入 -->
             <form-string-input
-              :config="config"
               :configuration="configuration[part][config]"
-              :index="partIndex"
+              :identifier="`${partIndex}-${config}`"
               :key="`${partIndex}-${config}`"
               v-else-if="configuration[part][config].type==='string'"
             />
 
             <!-- 单行输入 -->
             <form-url-input
-              :config="config"
               :configuration="configuration[part][config]"
-              :index="partIndex"
+              :identifier="`${partIndex}-${config}`"
               :key="`${partIndex}-${config}`"
               v-else-if="configuration[part][config].type==='url'"
             />
 
             <!-- 多行输入 -->
             <form-textarea-input
-              :config="config"
               :configuration="configuration[part][config]"
-              :index="partIndex"
+              :identifier="`${partIndex}-${config}`"
               :key="`${partIndex}-${config}`"
               v-else-if="configuration[part][config].type==='mutiline'"
             />
 
-            <!-- 列表 -->
-            <template v-else-if="configuration[part][config].type==='array'">
-              <a-form-item
-                :key="`${partIndex}-${config}-${index}`"
-                :required="index === 0"
-                v-bind="index === 0 ? LabelLayout : noLabelLayout"
-                v-for="(item, index) in form.getFieldValue(`${partIndex}-${config}-key`)"
-              >
-                <template #label v-if="index === 0">
-                  <!-- 表单项名称 -->
-                  {{configuration[part][config].title}}
-                  <!-- 描述文字 -->
-                  <a-tooltip
-                    :title="configuration[part][config].desc"
-                    v-if="configuration[part][config].desc"
-                  >
-                    <a-icon style="vertical-align:-0.125em;" type="question-circle"/>
-                  </a-tooltip>
-                </template>
-                <a-input
-                  style="width: 60%; margin-right: 8px"
-                  v-decorator="[
-                    `${partIndex}-${config}[${index}]`,
-                    {
-                      validateTrigger: ['change', 'blur'],
-                      preserve: true,
-                      rules: [{
-                          required: true,
-                          type: configuration[part][config].element,
-                          whitespace: true,
-                          message: 'Please fill this item some value or delete this field.',
-                      }]
-                    }
-                  ]"
-                />
-                <a-icon
-                  :disabled="form.getFieldValue(`${partIndex}-${config}-key`).length === 1"
-                  @click="() => remove(index,`${partIndex}-${config}-key`)"
-                  class="dynamic-delete-button"
-                  type="minus-circle-o"
-                  v-if="form.getFieldValue(`${partIndex}-${config}-key`).length > 1"
-                />
-              </a-form-item>
-              <a-form-item :key="`${partIndex}-${config}-add`" v-bind="noLabelLayout">
-                <a-button @click="add(`${partIndex}-${config}-key`)">
-                  <a-icon type="plus"/>新增项
-                </a-button>
-              </a-form-item>
-            </template>
-
-            <!-- 单个列表 -->
-            <a-form-item :key="`${partIndex}-${config}`" v-bind="LabelLayout" v-else>
-              <template #label>
-                <!-- 表单项名称 -->
-                {{configuration[part][config].title}}
-                <!-- 描述文字 -->
-                <a-tooltip
-                  :title="configuration[part][config].desc"
-                  v-if="configuration[part][config].desc"
-                >
-                  <a-icon style="vertical-align:-0.125em;" type="question-circle"/>
-                </a-tooltip>
-              </template>
-
-              <!-- 复合类型 -->
-              <template v-if="typeof configuration[part][config].type==='object'">
-                <!-- 选择需要的值类型 -->
-                <TypeSelect
-                  :option="configuration[part][config].type"
-                  v-model="unionTypeSelect[partIndex][config]"
-                />
-
-                <!-- 布尔值输入 -->
-                <a-radio-group
-                  :name="`${partIndex}-${config}`"
-                  :options=" [{ label: 'True', value: true }, { label: 'False', value: false }]"
-                  v-decorator="[
-                    `${partIndex}-${config}`,
-                    {
-                      initialValue: configuration[part][config].default,
-                      rules: [{
-                        required: configuration[part][config].required,
-                        type: 'boolean'
-                      }]
-                    }
-                  ]"
-                  v-if="unionTypeSelect[partIndex][config]==='boolean'"
-                />
-
-                <!-- 多行输入 -->
-                <a-textarea
-                  :autosize="{ minRows: 2 }"
-                  v-decorator="[
-                    `${partIndex}-${config}`,
-                    {
-                      initialValue: configuration[part][config].default,
-                      rules: [{
-                        required: configuration[part][config].required,
-                        type: 'string'
-                      }]
-                    }
-                  ]"
-                  v-else-if="unionTypeSelect[partIndex][config]==='mutiline'"
-                />
-
-                <!-- 单行输入 -->
-                <a-input
-                  v-decorator="[
-                    `${partIndex}-${config}`,
-                    {
-                      initialValue: configuration[part][config].default,
-                      rules: [{
-                        required: configuration[part][config].required,
-                        type: 'string'
-                      }]
-                    }
-                  ]"
-                  v-else-if="unionTypeSelect[partIndex][config]==='string'"
-                />
-
-                <!-- 数字输入 -->
-                <a-input-number
-                  :step="configuration[part][config].step"
-                  v-decorator="[
-                    `${partIndex}-${config}`,
-                    {
-                      initialValue: configuration[part][config].default,
-                      rules: [{
-                        required: configuration[part][config].required,
-                        type: 'number'
-                      }]
-                    }
-                  ]"
-                  v-else-if="unionTypeSelect[partIndex][config]==='number'"
-                />
-              </template>
-
-              <!-- 简单类型 -->
-              <template v-else>
-                <!-- 可遍历值输入 -->
-                <a-radio-group
-                  :name="`${partIndex}-${config}`"
-                  :options="configuration[part][config].enum"
-                  v-decorator="[
-                    `${partIndex}-${config}`,
-                    {
-                      initialValue: configuration[part][config].default,
-                      rules: [{
-                        required: configuration[part][config].required,
-                        type: configuration[part][config].type,
-                        enum: configuration[part][config].enum
-                      }]
-                    }
-                  ]"
-                  v-if="configuration[part][config].enum"
-                />
-
-                <!-- 对象输入 -->
-                <a-input
-                  v-decorator="[
-                    `${partIndex}-${config}`,
-                    {
-                      initialValue: configuration[part][config].default,
-                      rules: [{
-                        required: configuration[part][config].required,
-                        type: 'string'
-                      }]
-                    }
-                  ]"
-                  v-else-if="configuration[part][config].type==='object'"
-                />
-                <!-- 完善中... -->
-              </template>
-            </a-form-item>
+            <!-- 数组输入 -->
+            <form-array-input
+              :configuration="configuration[part][config]"
+              :identifier="`${partIndex}-${config}`"
+              :key="`${partIndex}-${config}`"
+              v-else-if="configuration[part][config].type==='array'"
+            />
           </template>
         </template>
       </template>
@@ -258,15 +97,16 @@
   </div>
 </template>
 <script lang="ts">
-import {
-  Component, Prop, Provide, Vue, Watch
-} from 'vue-property-decorator';
+import { Component, Prop, Provide, Vue } from 'vue-property-decorator';
 import DropdownTitle from '#/DropdownTitle.vue';
 import TypeSelect from '#/TypeSelect.vue';
+import FormArrayInput from '#/FormArrayInput.vue';
 import FormBooleanInput from '#/FormBooleanInput.vue';
+import FormEnumInput from '#/FormEnumInput.vue';
 import FormNumberInput from '#/FormNumberInput.vue';
 import FormStringInput from '#/FormStringInput.vue';
 import FormTextareaInput from '#/FormTextareaInput.vue';
+import FormUnionInput from '#/FormUnionInput.vue';
 import FormUrlInput from '#/FormUrlInput.vue';
 
 interface UnionTypeItem {
@@ -294,15 +134,16 @@ interface NormalObject {
   [propName: string]: any;
 }
 
-let id = 0;
-
 @Component({
   components: {
     DropdownTitle,
+    FormArrayInput,
     FormBooleanInput,
+    FormEnumInput,
     FormNumberInput,
     FormStringInput,
     FormTextareaInput,
+    FormUnionInput,
     FormUrlInput,
     TypeSelect
   }
@@ -332,33 +173,8 @@ export default class FormTest extends Vue {
     return tagList;
   }
 
-  // @Provide() public form = this.form;
-
   private beforeCreate() {
     this.form = this.$form.createForm(this);
-  }
-
-  private remove(index: number, formKey: string) {
-    // can use data-binding to get
-    const keys = this.form.getFieldValue(formKey);
-
-    // We need at least one item
-    if (keys.length === 1) return;
-
-    // can use data-binding to set
-    this.form.setFieldsValue({ [formKey]: keys.filter((key: number) => key !== index) });
-  }
-
-  private add(formKey: string) {
-    // can use data-binding to get
-    const keys = this.form.getFieldValue(formKey);
-
-    id += 1;
-    const nextKeys = keys.concat(id);
-    // can use data-binding to set
-
-    // important! notify form to detect changes
-    this.form.setFieldsValue({ [formKey]: nextKeys });
   }
 
   private handleSubmit(e: Event) {
@@ -379,20 +195,8 @@ export default class FormTest extends Vue {
           const index = Number(indexString);
           const value = formValue[x];
 
-          // 对additional类型做处理
-          if (additional) {
-            const originKeyValue: string = json[index][key];
-            const originKey = `${indexString}-${key}`;
-
-            if (additional === 'prefix')
-              if (originKeyValue) json[index][key] = `${value}${originKeyValue}`;
-              else formValue[originKey] = `${value}${formValue[originKey]}`;
-            else if (additional === 'suffix')
-              if (originKeyValue) json[index][key] += value;
-              else formValue[originKey] += value;
-
-            // 保证value有定义且不为默认值
-          } else if (typeof value !== 'undefined' && value !== this.configuration[json[index].tag][key].default)
+          // 保证value有定义且不为默认值
+          if (!additional && typeof value !== 'undefined' && value !== this.configuration[json[index].tag][key].default)
             json[index][key] = value;
         });
 
@@ -404,23 +208,6 @@ export default class FormTest extends Vue {
   // Add a new component in the end
   private addComponent() {
     this.tags.push('请选择');
-    // change uniontype length
-    this.unionTypeSelect.push({});
-  }
-
-  // check if the tag config has any array-type option
-  private tagChange(partIndex: number) {
-    const currentTag = this.tags[partIndex];
-
-    if (currentTag !== '请选择') {
-      const currentTagConfig = this.configuration[currentTag];
-
-      Object.keys(currentTagConfig).forEach(config => {
-        if (currentTagConfig[config].type === 'array')
-          this.form.getFieldDecorator(`${partIndex}-${config}-key`, { initialValue: [], preserve: true, rules: { type: 'array' } });
-      });
-    }
-
   }
 }
 </script>
