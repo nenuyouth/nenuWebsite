7af7b5bf09d1428d549faac012844422127cad57
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-08 02:47:07 +0000

    Deploy nenuyouth/nenuWebsite to github.com/nenuyouth/nenuWebsite.git:gh-pages

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("/service-worker/workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/service-worker/workbox-v4.3.1"});
+
+importScripts(
+  "/service-worker/precache-manifest.c95f6e4ff2abba983e4d53a3617e005c.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "innenu-website"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
